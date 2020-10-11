70fa904ea66b28d3852155831311200b6d36a97a
Yazeed962 <55327638+Yazeed962@users.noreply.github.com>
2020-10-11 20:29:03 +0400

    Update Projectile_Motion.py

--- Projectile_Motion.py
+++ Projectile_Motion.py
@@ -1,15 +1,6 @@
-"""
-Data: 
-angle = pi/4
-Drag coefficient = .30
-Initial velocity = 15 m/s
-mass = 2 kg
-radius = .2 meter
-initial hight = 0
-DENSITY = 1.225
-"""
 
-# RECURSIVELY UNTIL Y = 0
+
+# RUN RECURSIVELY UNTIL Y = 0
 
 import json
 from math import sin, cos, pi, radians
@@ -30,9 +21,9 @@ Y_INITIAL = data["Y_INITIAL"]
 DELTA_T = data["DELTA_T"]
 C = data["C"]
 
-ob = projectile_calculations(Y_INITIAL, V_INITIAL, DELTA_T, MASS, DIAMETER, C, THETA)
+simulation = projectile_calculations(Y_INITIAL, V_INITIAL, DELTA_T, MASS, DIAMETER, C, THETA)
 
-X = ob.run()
+results = simulation.run()
 
 
-print(X)
+print(results)
