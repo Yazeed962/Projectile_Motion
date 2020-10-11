a0caafae41e74b7ec25e8d0e0cffb751b4726c1f
Yazeed962 <55327638+Yazeed962@users.noreply.github.com>
2020-10-11 20:34:42 +0400

    Update Projectile_Motion.py

--- Projectile_Motion.py
+++ Projectile_Motion.py
@@ -3,7 +3,7 @@
 # RUN RECURSIVELY UNTIL Y = 0
 
 import json
-from math import sin, cos, pi, radians
+from math import radians
 from Projectile.util import projectile_calculations
 
 
@@ -12,16 +12,8 @@ from Projectile.util import projectile_calculations
 with open("Projectile\\data.json") as f:
     data = json.load(f)
 
-THETA= radians(data["THETA"])
-V_INITIAL = data["V_INITIAL"]
-MASS = data["MASS"]
-DIAMETER = data["DIAMETER"]
-DENSITY = data["DENSITY"]
-Y_INITIAL = data["Y_INITIAL"]
-DELTA_T = data["DELTA_T"]
-C = data["C"]
 
-simulation = projectile_calculations(Y_INITIAL, V_INITIAL, DELTA_T, MASS, DIAMETER, C, THETA)
+simulation = projectile_calculations(data["Y_INITIAL"], data["V_INITIAL"], data["DELTA_T"], data["MASS"], data["DIAMETER"], data["C"], data["THETA"])
 
 results = simulation.run()
 
