ccd00a03f2a1fbf013e398062fcbb6c4bbdf9a80
Yazeed962 <55327638+Yazeed962@users.noreply.github.com>
2020-10-11 21:01:58 +0400

    Update Projectile_Motion.py

--- Projectile_Motion.py
+++ Projectile_Motion.py
@@ -15,7 +15,17 @@ with open("Projectile\\data.json") as f:
 
 simulation = projectile_calculations(data["Y_INITIAL"], data["V_INITIAL"], data["DELTA_T"], data["MASS"], data["DIAMETER"], data["C"], data["THETA"])
 
-results = simulation.run()
+time, x_max, y_max = simulation.run()
 
+results = {
+    "time":time,
+    "x_max":x_max,
+    "y_max":y_max
+}
 
-print(results)
+with open('results.json', 'w') as res:
+    json.dump(results, res)
+
+print(f"""The trip time is {time} seconds
+The range is {x_max} meters
+The maximum hight is {y_max} meters""")
