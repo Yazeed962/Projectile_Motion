4092283df152a3a2dcb0591f2a99e8dcd1ed95bc
Yazeed962 <55327638+Yazeed962@users.noreply.github.com>
2020-10-13 15:58:51 +0400

    Update Projectile_Motion.py

--- Projectile_Motion.py
+++ Projectile_Motion.py
@@ -7,24 +7,27 @@ from Projectile.util import projectile_calculations
 
 
 
+def main():
+    with open("Projectile\\data.json") as f:
+        data = json.load(f)
 
-with open("Projectile\\data.json") as f:
-    data = json.load(f)
 
+    simulation = projectile_calculations(data["Y_INITIAL"], data["V_INITIAL"], data["DELTA_T"], data["MASS"], data["DIAMETER"], data["C"], data["THETA"])
 
-simulation = projectile_calculations(data["Y_INITIAL"], data["V_INITIAL"], data["DELTA_T"], data["MASS"], data["DIAMETER"], data["C"], data["THETA"])
+    time, x_max, y_max = simulation.run()
 
-time, x_max, y_max = simulation.run()
+    results = {
+        "time":time,
+        "x_max":x_max,
+        "y_max":y_max
+    }
 
-results = {
-    "time":time,
-    "x_max":x_max,
-    "y_max":y_max
-}
+    with open('results.json', 'w') as res:
+        json.dump(results, res)
 
-with open('results.json', 'w') as res:
-    json.dump(results, res)
+    print(f"""The trip time is {time} seconds
+    The range is {x_max} meters
+    The maximum hight is {y_max} meters""")
 
-print(f"""The trip time is {time} seconds
-The range is {x_max} meters
-The maximum hight is {y_max} meters""")
+    if __name__ == '__main__':
+        main()
